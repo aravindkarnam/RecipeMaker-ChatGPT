3e5f3198d388221b5f393327dd46ab95d51d2677
Aravind Karnam <aravind.karanam@gmail.com>
2023-05-05 13:35:46 +0530

    added cors

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var recipesRouter = require('./routes/recipes');
 
 var app = express();
-
+app.use(cors({
+    origin: 'https://recipegpt.onrender.com'
+  }))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
